2a3279e96cf3e834e8fb3fde2472726154378379
Jason Sanmiya <jsanmiya@google.com>
2015-07-07 15:24:50 -0700

    Improve banks.
    New banks are lower near the river, and higher near the edge,
    so that you cannot see the horizon, but the ground is still
    mostly flat. This creates a definite foreground.
    
    Banks are now created from data in config.json, instead
    of hard-coded numbers in the code.
    
    The banks can shift in their width as well.
    
    The creation algorithm has been cleaned-up and documented.
    
    Polygons no longer go overlap around sharp corners.
    
    Texture mapping is calculated instead of being hard-coded.
    
    Tested: Works on Linux.
    Change-Id: Ib2f20d48ee5db7fca14f63284e3dfbcbfb06a907

--- src/components/river.cpp
+++ src/components/river.cpp
@@ -22,14 +22,18 @@
 #include <math.h>
 #include <memory>
 
+using mathfu::vec2;
 using mathfu::vec3;
 using mathfu::quat;
+using mathfu::kAxisZ3f;
 
 FPL_ENTITY_DEFINE_COMPONENT(fpl::fpl_project::RiverComponent,
                             fpl::fpl_project::RiverData)
 
 namespace fpl {
 namespace fpl_project {
+
+static const size_t kNumIndicesPerQuad = 6;
 
 void RiverComponent::InitEntity(entity::EntityRef& entity) {
   entity_manager_->AddEntityToComponent<RenderMeshComponent>(entity);
@@ -61,23 +65,27 @@ void RiverComponent::CreateRiverMesh(entity::EntityRef& entity) {
   static const Attribute kMeshFormat[] = {kPosition3f, kTexCoord2f, kNormal3f,
                                           kTangent4f, kEND};
   std::vector<mathfu::vec3_packed> track;
-
-  const Config* config =
-      entity_manager_->GetComponent<ServicesComponent>()->config();
+  const RiverConfig* river = entity_manager_->GetComponent<ServicesComponent>()
+                                 ->config()
+                                 ->river_config();
 
   Rail* rail = entity_manager_->GetComponent<ServicesComponent>()
                    ->rail_manager()
-                   ->GetRail(config->river_config()->rail_filename()->c_str());
+                   ->GetRail(river->rail_filename()->c_str());
 
   // Generate the spline data and store it in our track vector:
-  rail->Positions(config->river_config()->spline_stepsize(), &track);
+  rail->Positions(river->spline_stepsize(), &track);
 
-  size_t segment_count = track.size();
-  size_t river_vert_max = segment_count * 2;
-  size_t river_index_max = (segment_count - 1) * 6;
-  size_t bank_segments = 3;
-  size_t bank_vert_max = segment_count * 2 * (bank_segments + 1);
-  size_t bank_index_max = (segment_count - 1) * 6 * (bank_segments * 2);
+  const size_t num_bank_contours = river->banks()->Length();
+  const size_t num_bank_quads = num_bank_contours - 2;
+  const size_t river_idx = river->river_index();
+  const size_t segment_count = track.size();
+  const size_t river_vert_max = segment_count * 2;
+  const size_t river_index_max = (segment_count - 1) * kNumIndicesPerQuad;
+  const size_t bank_vert_max = segment_count * num_bank_contours;
+  const size_t bank_index_max =
+      (segment_count - 1) * kNumIndicesPerQuad * num_bank_quads;
+  assert(num_bank_contours >= 2 && river_idx < num_bank_contours - 1);
 
   // Need to allocate some space to plan out our mesh in:
   std::vector<NormalMappedVertex> river_verts(river_vert_max);
@@ -90,83 +98,123 @@ void RiverComponent::CreateRiverMesh(entity::EntityRef& entity) {
   std::vector<unsigned short> bank_indices(bank_index_max);
   bank_indices.clear();
 
-  // the normal to where we are on the track right now.  Initialized to
-  // our best guess, based on the first two points.
-  vec3 track_normal =
-      vec3::CrossProduct(vec3(track[1]) - vec3(track[0]), mathfu::kAxisZ3f)
-          .Normalized();
-
   // Construct the actual mesh data for the river:
+  std::vector<vec2> offsets(num_bank_contours);
   for (size_t i = 0; i < segment_count; i++) {
-    if (i != 0) {
-      track_normal = vec3::CrossProduct(vec3(track[i]) - vec3(track[i - 1]),
-                                        mathfu::kAxisZ3f)
-                         .Normalized();
+    // River track is circular.
+    const size_t prev_i = i == 0 ? segment_count - 1 : i - 1;
+
+    // Get the current position on the track, and the normal (to the side).
+    const vec3 track_delta = vec3(track[i]) - vec3(track[prev_i]);
+    const vec3 track_normal =
+        vec3::CrossProduct(track_delta, kAxisZ3f).Normalized();
+    const vec3 track_position =
+        vec3(track[i]) + river->track_height() * kAxisZ3f;
+
+    // The river texture is tiled several times along the course of the river.
+    // TODO: Change this from tile count to actual physical size for a tile.
+    //       Requires that we know the total path distance.
+    const float texture_v = river->texture_tile_size() * static_cast<float>(i) /
+                            static_cast<float>(segment_count);
+
+    // Get the (side, up) offsets of the bank vertices.
+    // The offsets are relative to `track_position`.
+    // side == distance along `track_normal`
+    // up == distance along kAxisZ3f
+    for (size_t j = 0; j < num_bank_contours; ++j) {
+      const RiverBankContour* b = river->banks()->Get(j);
+      offsets[j] =
+          vec2(mathfu::Lerp(b->x_min(), b->x_max(), mathfu::Random<float>()),
+               mathfu::Lerp(b->z_min(), b->z_max(), mathfu::Random<float>()));
     }
-    track_normal *= config->river_config()->track_half_width();
-    vec3 track_pos = vec3(track[i]);
-    float texture_y = config->river_config()->texture_tile_size() *
-                      static_cast<float>(i) / static_cast<float>(segment_count);
 
-    auto make_vert = [&](std::vector<NormalMappedVertex>& verts, float xpos,
-                         float xtc, float ypos) {
-      verts.push_back(NormalMappedVertex{
-          vec3_packed(
-              vec3(track_pos + track_normal * xpos) +
-              vec3(0.0f, 0.0f, config->river_config()->track_height() + ypos)),
-          vec2_packed(vec2(xtc, texture_y)), vec3_packed(vec3(0, 1, 0)),
-          vec4_packed(vec4(1, 0, 0, 1)),
+    // Create the bank vertices for this segment.
+    for (size_t j = 0; j < num_bank_contours; ++j) {
+      const vec2 off = offsets[j];
+      const vec3 vertex =
+          track_position + off.x() * track_normal + off.y() * kAxisZ3f;
+
+      // The texture is stretched from the side of the river to the far end
+      // of the bank. There are two banks, however, separated by the river.
+      // We need to know the width of the bank to caluate the `texture_u`
+      // coordinate.
+      const bool left_bank = j <= river_idx;
+      const size_t bank_start = left_bank ? 0 : river_idx + 1;
+      const size_t bank_end = left_bank ? river_idx : num_bank_contours - 1;
+      const float bank_width = offsets[bank_start].x() - offsets[bank_end].x();
+      const float texture_u = (off.x() - offsets[bank_end].x()) / bank_width;
+
+      bank_verts.push_back(NormalMappedVertex{
+          vec3_packed(vertex), vec2_packed(vec2(texture_u, texture_v)),
+          vec3_packed(vec3(0, 1, 0)), vec4_packed(vec4(1, 0, 0, 1)),
       });
-    };
+    }
 
-    make_vert(river_verts, -1, 0, 0);
-    make_vert(river_verts, 1, 1, 0);
-
-    make_vert(bank_verts, -3.0f, -2.0f, 0.5f + mathfu::Random<float>() * 0.7f);
-    make_vert(bank_verts, -2.0f, -1.5f, 0.7f + mathfu::Random<float>() * 0.7f);
-    make_vert(bank_verts, -1.2f, -1.2f, 0.3f + mathfu::Random<float>() * 0.7f);
-    make_vert(bank_verts, -1.0f, -1.0f, 0.0f);  // Matches river vert.
-    make_vert(bank_verts, 1.0f, 1.0f, 0.0f);    // Matches river vert.
-    make_vert(bank_verts, 1.2f, 1.2f, 0.3f + mathfu::Random<float>() * 0.7f);
-    make_vert(bank_verts, 2.0f, 1.5f, 0.7f + mathfu::Random<float>() * 0.7f);
-    make_vert(bank_verts, 3.0f, 2.0f, 0.5f + mathfu::Random<float>() * 0.7f);
+    // Ensure vertices don't go behind previous vertices on the inside of
+    // a tight corner.
+    if (i > 0) {
+      const NormalMappedVertex* prev_verts =
+          &bank_verts[bank_verts.size() - 2 * num_bank_contours];
+      NormalMappedVertex* cur_verts =
+          &bank_verts[bank_verts.size() - num_bank_contours];
+      for (size_t j = 0; j < num_bank_contours; j++) {
+        const vec3 vert_delta =
+            vec3(cur_verts[j].pos) - vec3(prev_verts[j].pos);
+        const float dot = vec3::DotProduct(vert_delta, track_delta);
+        const bool cur_vert_goes_backwards_along_track = dot <= 0.0f;
+        if (cur_vert_goes_backwards_along_track) {
+          cur_verts[j].pos = vec3(prev_verts[j].pos) + 0.000001f * track_delta;
+        }
+      }
+    }
 
     // Force the beginning and end to line up in their geometry:
     if (i == segment_count - 1) {
-      river_verts[river_verts.size() - 2].pos = river_verts[0].pos;
-      river_verts[river_verts.size() - 1].pos = river_verts[1].pos;
-
-      for (size_t j = 0; j < 8; j++)
+      for (size_t j = 0; j < num_bank_contours; j++)
         bank_verts[bank_verts.size() - (8 - j)].pos = bank_verts[j].pos;
     }
 
-    // Not counting the first segment, create triangles in our index
-    // list to represent this segment.
-    if (i != 0) {
-      auto make_quad = [&](std::vector<unsigned short>& indices,
-                           std::vector<NormalMappedVertex>& verts, int off1,
-                           int off2) {
-        indices.push_back(verts.size() - 2 - off2);
-        indices.push_back(verts.size() - 1 - off2);
-        indices.push_back(verts.size() - 2 - off1);
+    // The river has two of the middle vertices of the bank.
+    // The texture coordinates are different, however.
+    const size_t river_vert = bank_verts.size() - num_bank_contours + river_idx;
+    river_verts.push_back(bank_verts[river_vert]);
+    river_verts.back().tc = vec2(0.0f, texture_v);
+    river_verts.push_back(bank_verts[river_vert + 1]);
+    river_verts.back().tc = vec2(1.0f, texture_v);
+  }
 
-        indices.push_back(verts.size() - 2 - off1);
-        indices.push_back(verts.size() - 1 - off2);
-        indices.push_back(verts.size() - 1 - off1);
-      };
+  // Not counting the first segment, create triangles in our index
+  // list to represent this segment.
+  //
+  for (size_t i = 0; i < segment_count - 1; i++) {
+    auto make_quad = [&](std::vector<unsigned short>& indices, int base_index,
+                         int off1, int off2) {
+      indices.push_back(base_index + off1);
+      indices.push_back(base_index + off1 + 1);
+      indices.push_back(base_index + off2);
 
-      make_quad(river_indices, river_verts, 0, 2);
+      indices.push_back(base_index + off2);
+      indices.push_back(base_index + off1 + 1);
+      indices.push_back(base_index + off2 + 1);
+    };
 
-      make_quad(bank_indices, bank_verts, 0, 8);
-      make_quad(bank_indices, bank_verts, 1, 9);
-      make_quad(bank_indices, bank_verts, 2, 10);
-      make_quad(bank_indices, bank_verts, 4, 12);
-      make_quad(bank_indices, bank_verts, 5, 13);
-      make_quad(bank_indices, bank_verts, 6, 14);
+    // River only has one quad per segment.
+    make_quad(river_indices, 2 * i, 0, 2);
+
+    // Case when kNumBankCountours = 8, and river_idx = 3;
+    //
+    //  0___1___2___3   4___5___6___7
+    //  | _/| _/| _/|   | _/| _/| _/|
+    //  |/__|/__|/__|   |/__|/__|/__|
+    //  8   9  10  11  12  13  14  15
+    for (size_t j = 0; j <= num_bank_quads; ++j) {
+      // Do not create bank geo for the river.
+      if (j == river_idx) continue;
+      make_quad(bank_indices, i * num_bank_contours, j, num_bank_contours + j);
     }
   }
 
-  // Make sure we used as much data as expected, and no more.  (Or less!)
+  // Make sure we used as much data as expected, and no more.
   assert(river_indices.size() == river_index_max);
   assert(river_verts.size() == river_vert_max);
   assert(bank_indices.size() == bank_index_max);
@@ -180,7 +228,7 @@ void RiverComponent::CreateRiverMesh(entity::EntityRef& entity) {
 
   // Load the material from files.
   Material* river_material =
-      asset_manager->LoadMaterial(config->river_config()->material()->c_str());
+      asset_manager->LoadMaterial(river->material()->c_str());
   Material* bank_material =
       asset_manager->LoadMaterial("materials/ground_material.fplmat");
 
@@ -200,8 +248,7 @@ void RiverComponent::CreateRiverMesh(entity::EntityRef& entity) {
 
   // Add the river mesh to the river entity.
   RenderMeshData* mesh_data = Data<RenderMeshData>(entity);
-  mesh_data->shader =
-      asset_manager->LoadShader(config->river_config()->shader()->c_str());
+  mesh_data->shader = asset_manager->LoadShader(river->shader()->c_str());
   mesh_data->mesh = river_mesh;
   mesh_data->ignore_culling = true;  // Never cull the river.
   mesh_data->pass_mask = 1 << RenderPass_kOpaque;
@@ -229,3 +276,4 @@ void RiverComponent::CreateRiverMesh(entity::EntityRef& entity) {
 
 }  // fpl_project
 }  // fpl
+
