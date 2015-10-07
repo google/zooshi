94a02092703dc8280ee279496e5fd7fa692c2ce8
Jason Sanmiya <jsanmiya@google.com>
2015-10-06 17:57:23 -0700

    Fix file headers. Remove empty test.
    Fixed licenses in several file headers.
    
    Change-Id: Ifcc6511ac617e73149b7c743fb52eeeb5d6e4297
    Tested: Linux build still works.

--- jni/build_flatc.bat
+++ jni/build_flatc.bat
@@ -1,18 +1,16 @@
-@rem Copyright (c) 2013 Google, Inc.
+@rem Copyright 2013 Google Inc. All rights reserved.
 @rem
-@rem This software is provided 'as-is', without any express or implied
-@rem warranty.  In no event will the authors be held liable for any damages
-@rem arising from the use of this software.
-@rem Permission is granted to anyone to use this software for any purpose,
-@rem including commercial applications, and to alter it and redistribute it
-@rem freely, subject to the following restrictions:
-@rem 1. The origin of this software must not be misrepresented; you must not
-@rem claim that you wrote the original software. If you use this software
-@rem in a product, an acknowledgment in the product documentation would be
-@rem appreciated but is not required.
-@rem 2. Altered source versions must be plainly marked as such, and must not be
-@rem misrepresented as being the original software.
-@rem 3. This notice may not be removed or altered from any source distribution.
+@rem Licensed under the Apache License, Version 2.0 (the "License");
+@rem you may not use this file except in compliance with the License.
+@rem You may obtain a copy of the License at
+@rem
+@rem     http://www.apache.org/licenses/LICENSE-2.0
+@rem
+@rem Unless required by applicable law or agreed to in writing, software
+@rem distributed under the License is distributed on an "AS IS" BASIS,
+@rem WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+@rem See the License for the specific language governing permissions and
+@rem limitations under the License.
 @echo off
 
 setlocal enabledelayedexpansion
