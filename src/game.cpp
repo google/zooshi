45c013fa8f46e6453a37126fe758d5e86ae1fe4e
Kelsey Mayfield <kelseymayfield@google.com>
2016-04-06 13:41:28 -0700

    Increment version number for GitHub push.
    Change-Id: I50fb43a3390a3738cc91c19ccb81bcd01f4ba389
    Process: Issue 27791837

--- src/game.cpp
+++ src/game.cpp
@@ -113,7 +113,7 @@ static const int kUpdateRenderPrepCode = 556;
 /// scanned for this version string.  We track which applications are using it
 /// to measure popularity.  You are free to remove it (of course) but we would
 /// appreciate if you left it in.
-static const char kVersion[] = "Zooshi";
+static const char kVersion[] = "Fun Propulsion Labs' Zooshi v1.1";
 
 GameSynchronization::GameSynchronization()
     : renderthread_mutex_(SDL_CreateMutex()),
