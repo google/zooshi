abe05c13b9ce8bba39b75cc114a132785a0abed5
Stewart Miles <smiles@google.com>
2016-06-02 08:41:19 -0700

    Removed deprecated armeabi-v7a-hard Android ABI.
    Change-Id: Ib6a57f73194a221f68a64a7b1ede83c38217c761

--- jni/Application.mk
+++ jni/Application.mk
@@ -3,9 +3,6 @@
 # See CPLUSPLUS-SUPPORT.html in the NDK documentation for more information
 # APP_STL := stlport_static
 APP_STL := gnustl_static
-
-APP_ABI := armeabi-v7a-hard x86 armeabi
-
+APP_ABI := armeabi-v7a x86 armeabi
 APP_CPPFLAGS += -std=c++11 -Wno-literal-suffix -Wno-unused-function
-
 APP_PLATFORM := android-15
